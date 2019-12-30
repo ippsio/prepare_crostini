82856923c882bb4b12d6b8bc590e7ec3826531b9
ippsio <ippei.shioiri@gmail.com>
2019-12-30 11:50:48 +0900

    tzdata non-interctive

--- 2.sh
+++ 2.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-sudo dpkg-reconfigure tzdata
+
+echo 'Asia/Tokyo' | sudo tee /etc/timezone
+sudo dpkg-reconfigure --frontend noninteractive tzdata
+#sudo dpkg-reconfigure tzdata
 sudo apt -y install task-japanese locales-all fonts-ipafont
 sudo localectl set-locale LANG=ja_JP.UTF-8 LANGUAGE="ja_JP:ja"
 source /etc/default/locale
